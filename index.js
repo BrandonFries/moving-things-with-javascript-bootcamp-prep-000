1a0de5a806797acad1dc01c1f14c00930637f042
Brandon Fries <brandonfries1993@gmail.com>
2019-07-16 19:00:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const app = "I don't do much."
 document.addEventListener('keydown', function(e) {
-  console.log(e.which)
+  if (e.which === 37) {
+    var leftNumbers = dodger.style.left.replace('px', '')
+    var left = parseInt(leftNumbers, 10)
+ 
+    dodger.style.left = `${left - 1}px`
+  }
 })
